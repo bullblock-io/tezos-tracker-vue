10cab3a4092979179ae348803fe40b1c331a63b3
Roman Bolgov <bolgovr@codex.one>
2019-07-26 12:25:25 +0300

    move promo footer to index page

--- src/components/menu/footer.vue
+++ src/components/menu/footer.vue
@@ -1,49 +1,5 @@
 <template>
   <div>
-    <div class="promo-footer">
-      <div class="container-fluid">
-        <div class="row justify-content-md-center align-items-center">
-          <div class="col-lg-4">
-            <img src="img/footer-img.svg" width="85%" alt />
-          </div>
-          <div class="col-lg-6">
-            <div class="row">
-              <div class="col-lg-12">
-                <h2>More about Tezos</h2>
-              </div>
-            </div>
-            <div class="row">
-              <div class="col-lg-3">
-                <ul>
-                  <li>
-                    <a href="#">What is Tezos?</a>
-                  </li>
-                  <li>
-                    <a href="#">White Paper</a>
-                  </li>
-                  <li>
-                    <a href="#">Testnet Faucet</a>
-                  </li>
-                </ul>
-              </div>
-              <div class="col-lg-3">
-                <ul>
-                  <li>
-                    <a href="#">How to Run Tezos?</a>
-                  </li>
-                  <li>
-                    <a href="#">Git repository</a>
-                  </li>
-                  <li>
-                    <a href="#">Docker images</a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
     <footer class="footer">
       <div class="container-fluid">
         <div class="row justify-content-md-center align-items-center">
